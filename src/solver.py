2d0203ee06db42d975b9400bf92ce1ba0997974e
ehan03 <eugeneh442@gmail.com>
2024-05-16 01:28:46 -0700

    finished solver

---
+++ src/solver.py
@@ -0,0 +1,41 @@
+# standard library imports
+import os
+from pathlib import Path
+from typing import List, Set
+
+# third party imports
+import numpy as np
+
+# local imports
+
+
+class LinkedInQueensSolver:
+    """
+    Class to brute force the daily LinkedIn Queens game
+    """
+
+    def __init__(self, board: np.ndarray) -> None:
+        self.board = board
+        self.board_size = board.shape[0]
+        self.solution = []
+
+    def __get_solutions(self, arr: List[int], seen_colors: Set) -> None:
+        if len(arr) == self.board_size:
+            self.solution.append(arr)
+            return
+
+        for i in range(self.board_size):
+            if (not arr) or (
+                i not in arr
+                and abs(arr[-1] - i) != 1
+                and self.board[len(arr)][i] not in seen_colors
+            ):
+                self.__get_solutions(arr + [i], seen_colors | {self.board[len(arr)][i]})
+
+    def solve(self) -> List[int]:
+        self.__get_solutions([], set())
+
+        if not self.solution:
+            raise ValueError("No solution found")
+
+        return self.solution[0]
